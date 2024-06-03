0e2f7ae970663eed7dfc213922c795b4884a8821
Thiago Silva <165035188+tsilvadev89@users.noreply.github.com>
2024-06-03 09:49:31 -0300

    Update

--- frontend/vendas/vite.config.ts
+++ frontend/vendas/vite.config.ts
@@ -1,7 +1,31 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
+
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: true,
+    port: 80,
+    },
+});
+
+
+
+
+
+
+
+
+/* export default defineConfig({
+  plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: '34.229.141.155', // Use o endereço IP público da sua instância EC2
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
+}); */
