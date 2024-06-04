82d2d7e512ecd37b73ed6085bf1e9446826d6947
Thiago Silva <165035188+tsilvadev89@users.noreply.github.com>
2024-06-04 12:08:53 -0300

    Update

--- frontend/vendas/vite.config.ts
+++ frontend/vendas/vite.config.ts
@@ -18,13 +18,13 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: '54.196.168.75', // ENDEREÇO PUBLICO DA APLICAÇÃO
+        target: '54.234.79.240', // ENDEREÇO PUBLICO DA APLICAÇÃO
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
     },
   },
-  build: {
+/*   build: {
     outDir: '/var/www/html', // Define o diretório de saída para a pasta do Apache
-  },
+  }, */
 });
